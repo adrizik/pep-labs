59239bdf3081587978e2aa84cdbc06549acb75d8
adrizik <rizik_adriana@hotmail.com>
2023-02-04 22:46:50 +0000

    Saved progress on WordCount 2023-02-04 22:46:50 +0000

--- WordCount/src/main/java/ReturnWordCount.java
+++ WordCount/src/main/java/ReturnWordCount.java
@@ -5,6 +5,10 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        
+        int result = in.split(" ").length;
+        return result;
+
+
     }
 }
